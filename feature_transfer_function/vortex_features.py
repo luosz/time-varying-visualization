57dd0d95485ce21b17dc516f7c07d13219713379
Joe <lsz2006.ad@gmail.com>
2015-10-13 22:47:10 +0100

    optimize transfer functions for the vortex data set

---
+++ feature_transfer_function/vortex_features.py
@@ -0,0 +1,18 @@
+# Voreen Python script
+import voreen
+import voreenqt
+
+voreen.info()
+voreenqt.info()
+
+for i in range(0, 98):
+    voreen.setPropertyValue("VolumeSelector", "volumeID", i)
+    # tf_filename = "../../../../_uchar/vortex/%02d.tfi" % i
+    # voreen.loadTransferFunction("SingleVolumeRaycaster", "transferFunction", tf_filename)
+    # print "loaded transfer function %s" % tf_filename
+    voreen.repaint()
+    # image_filename = "../../../../output/vortex_output/%02d.png" % i
+    # voreen.snapshotCanvas(0, image_filename)
+    # print "saved to image %s" % image_filename
+
+voreen.setPropertyValue("VolumeSelector", "volumeID", 0)
