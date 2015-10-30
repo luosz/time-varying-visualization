f5c1184ad3e21802972eeebe5279438855613068
Joe <lsz2006.ad@gmail.com>
2015-10-30 23:35:44 +0000

    add time-varying_saliency.nb

--- feature_transfer_function/vortex_features.py
+++ feature_transfer_function/vortex_features.py
@@ -5,14 +5,22 @@ import voreenqt
 voreen.info()
 voreenqt.info()
 
+index=range(1,24)+range(25,100)
 for i in range(0, 98):
     voreen.setPropertyValue("VolumeSelector", "volumeID", i)
     # tf_filename = "../../../../_uchar/vortex/%02d.tfi" % i
     # voreen.loadTransferFunction("SingleVolumeRaycaster", "transferFunction", tf_filename)
     # print "loaded transfer function %s" % tf_filename
+    tf_filename="D:/document/work/time-varying-visualization/~plot/vorts%d_optimized_parallelsearch.tfi" % index[i]
+    voreen.loadTransferFunction("SingleVolumeRaycaster", "transferFunction", tf_filename)
+    print("loaded transfer function %s" % tf_filename)
+
     voreen.repaint()
     # image_filename = "../../../../output/vortex_output/%02d.png" % i
     # voreen.snapshotCanvas(0, image_filename)
     # print "saved to image %s" % image_filename
+    image_filename="D:/document/work/time-varying-visualization/~rendering2/%02d.png" % index[i]
+    voreen.snapshotCanvas(0, image_filename)
+    print("saved to image %s" % image_filename)
 
 voreen.setPropertyValue("VolumeSelector", "volumeID", 0)
